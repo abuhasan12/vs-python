eb6e36fe79498e8add6e05269389dccae1c0ce31
abuhasan12 <HASAN-MA3@ULSTER.AC.UK>
2026-02-06 00:51:49 +0000

    initial commit vs python

---
+++ Even or Odd.py
@@ -0,0 +1,7 @@
+height = float(input("Enter your height: "))
+
+
+if height % 2 == 0:
+  print("Even height")
+else:
+  print("odd height")
---
+++ minutes to hours.py
@@ -0,0 +1,3 @@
+n = int(input("enter minutes: "))
+hours = n/60
+print("here is converted hours=", hours)
