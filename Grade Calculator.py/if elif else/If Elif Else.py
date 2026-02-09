0cd1c74bde9fa254dacb3ec47f91fe14033b10d8
abuhasan12 <HASAN-MA3@ULSTER.AC.UK>
2026-02-09 02:02:56 +0000

    Initial Commit Vs Python

---
+++ Grade Calculator.py/if elif else/If Elif Else.py
@@ -0,0 +1,9 @@
+marks = float(input("enter marks"))
+if marks<0 or marks>100:
+  print("INVALID MARKS")
+elif marks>=50:
+  print("PASS")
+else:
+  print("FAIL")
+
+ 
---
+++ Grade Calculator.py/if elif else/README.md
@@ -0,0 +1,7 @@
+Write a program that asks the user to enter marks (0–100) and prints:
+
+Pass (marks ≥ 50)
+
+Fail (marks < 50)
+
+Invalid marks (outside 0–100)
